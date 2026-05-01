a2a73b83d49d396214eb445c81b8fa9a5fd261cf
User 77221 <77221@example.com>
2026-05-01 22:51:53 +0800

    Initial Kotlin project with Main.kt and Person.kt

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main() { 
+    println("Hello, Kotlin!") 
+    println("Git 版本控制已就绪!") 
+} 
---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,3 @@
+class Person(val name: String, val age: Int) { 
+    fun introduce() = "My name is %name%, I'm %age% years old" 
+} 
